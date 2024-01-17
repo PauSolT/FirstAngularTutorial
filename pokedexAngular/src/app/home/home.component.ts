8b10809273a44700305ff3f40e658fe9787a4674
pau <pausoltor@gmail.com>
2024-01-17 11:37:49 +0100

    Fix interface route

--- pokedexAngular/src/app/home/home.component.ts
+++ pokedexAngular/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { HousingLocation } from '../housinglocation';
+import { HousingLocation } from '../Interfaces/housinglocation';
 
 @Component({
   selector: 'app-home',
