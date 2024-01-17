e1a7dfbb3bd468c73d0ee9634ffb5b84886fd958
pau <pausoltor@gmail.com>
2024-01-17 11:36:58 +0100

    Create interface folder

--- pokedexAngular/src/app/home/home.component.ts
+++ pokedexAngular/src/app/home/home.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import {HousingLocation} from '../housinglocation';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-
 export class HomeComponent {
   readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
   housingLocation: HousingLocation = {
