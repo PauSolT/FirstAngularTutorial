3583866c97edac2f39017b8572a60c9835463b66
alexmrtc <alexmrtc@gmail.com>
2024-01-17 12:41:39 +0100

    Integrate details page into application

--- pokedexAngular/src/app/app.component.ts
+++ pokedexAngular/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 // import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
